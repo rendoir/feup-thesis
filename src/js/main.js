81f7d915dacf3dfaf4c69771146f98e461d63a84
rendoir <danfil.marques@gmail.com>
2020-02-19 12:45:36 +0000

    Adds example for multiple scenes

--- src/js/main.js
+++ src/js/main.js
@@ -1,46 +1,146 @@
 var THREE = require('three');
 
-class VisualNarrative {
-    constructor() {
-        this.init();
-        this.animate();
+var canvas;
+
+var scenes = [], renderer;
+
+init();
+animate();
+
+function init() {
+
+    canvas = document.getElementById("canvas");
+
+    var geometries = [
+        new THREE.BoxBufferGeometry(1, 1, 1),
+        new THREE.SphereBufferGeometry(0.5, 12, 8),
+        new THREE.DodecahedronBufferGeometry(0.5),
+        new THREE.CylinderBufferGeometry(0.5, 0.5, 1, 12)
+    ];
+
+    var template = document.getElementById("template").text;
+    var content = document.getElementById("content");
+
+    for (var i = 0; i < 40; i++) {
+
+        var scene = new THREE.Scene();
+
+        // make a list item
+        var element = document.createElement("div");
+        element.className = "list-item";
+        element.innerHTML = template.replace('$', i + 1);
+
+        // Look up the element that represents the area
+        // we want to render the scene
+        scene.userData.element = element.querySelector(".scene");
+        content.appendChild(element);
+
+        var camera = new THREE.PerspectiveCamera(50, 1, 1, 10);
+        camera.position.z = 2;
+        scene.userData.camera = camera;
+
+        // add one random mesh to each scene
+        var geometry = geometries[geometries.length * Math.random() | 0];
+
+        var material = new THREE.MeshStandardMaterial({
+
+            color: new THREE.Color().setHSL(Math.random(), 1, 0.75),
+            roughness: 0.5,
+            metalness: 0,
+            flatShading: true
+
+        });
+
+        scene.add(new THREE.Mesh(geometry, material));
+
+        scene.add(new THREE.HemisphereLight(0xaaaaaa, 0x444444));
+
+        var light = new THREE.DirectionalLight(0xffffff, 0.5);
+        light.position.set(1, 1, 1);
+        scene.add(light);
+
+        scenes.push(scene);
+
     }
 
-    init() {
-        this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-        this.camera.position.z = 1;
-    
-        this.scene = new THREE.Scene();
-    
-        this.geometry = new THREE.BoxGeometry( 0.5, 0.5, 0.5 );
-        this.material = new THREE.MeshNormalMaterial();
-    
-        this.mesh = new THREE.Mesh( this.geometry, this.material );
-        this.scene.add( this.mesh );
-    
-        this.renderer = new THREE.WebGLRenderer( { antialias: true } );
-        this.renderer.setSize( window.innerWidth, window.innerHeight );
-        document.body.appendChild( this.renderer.domElement );
-    
-        window.addEventListener( 'resize', this.onWindowResize.bind(this), false );
-    }
 
-    animate() {
-        this.merda = 
-        requestAnimationFrame( this.animate.bind(this) );
-    
-        this.mesh.rotation.x += 0.01;
-        this.mesh.rotation.y += 0.02;
-    
-        this.renderer.render( this.scene, this.camera );
-    }
+    renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true });
+    renderer.setClearColor(0xffffff, 1);
+    renderer.setPixelRatio(window.devicePixelRatio);
 
-    onWindowResize() {
-        this.camera.aspect = window.innerWidth / window.innerHeight;
-        this.camera.updateProjectionMatrix();
-    
-        this.renderer.setSize( window.innerWidth, window.innerHeight );
-    }
 }
 
-var visualNarrative = new VisualNarrative();
+function updateSize() {
+
+    var width = canvas.clientWidth;
+    var height = canvas.clientHeight;
+
+    if (canvas.width !== width || canvas.height !== height) {
+
+        renderer.setSize(width, height, false);
+
+    }
+
+}
+
+function animate() {
+
+    render();
+    requestAnimationFrame(animate);
+
+}
+
+function render() {
+
+    updateSize();
+
+    canvas.style.transform = `translateY(${window.scrollY}px)`;
+
+    renderer.setClearColor(0xffffff);
+    renderer.setScissorTest(false);
+    renderer.clear();
+
+    renderer.setClearColor(0xe0e0e0);
+    renderer.setScissorTest(true);
+
+    scenes.forEach(function (scene) {
+
+        // so something moves
+        scene.children[0].rotation.y = Date.now() * 0.001;
+
+        // get the element that is a place holder for where we want to
+        // draw the scene
+        var element = scene.userData.element;
+
+        // get its position relative to the page's viewport
+        var rect = element.getBoundingClientRect();
+
+        // check if it's offscreen. If so skip it
+        if (rect.bottom < 0 || rect.top > renderer.domElement.clientHeight ||
+            rect.right < 0 || rect.left > renderer.domElement.clientWidth) {
+
+            return; // it's off screen
+
+        }
+
+        // set the viewport
+        var width = rect.right - rect.left;
+        var height = rect.bottom - rect.top;
+        var left = rect.left;
+        var bottom = renderer.domElement.clientHeight - rect.bottom;
+
+        renderer.setViewport(left, bottom, width, height);
+        renderer.setScissor(left, bottom, width, height);
+
+        var camera = scene.userData.camera;
+
+        //camera.aspect = width / height; // not changing in this example
+        //camera.updateProjectionMatrix();
+
+        //scene.userData.controls.update();
+
+        renderer.render(scene, camera);
+
+    });
+
+}
